a941b0f7354355a39855b37128bae02ca265c945
texashodlr <conorxdevlin@gmail.com>
2025-06-03 20:44:45 -0600

    Cleaned up Map

--- src/network_graph.py
+++ src/network_graph.py
@@ -21,12 +21,54 @@ fig = go.Figure()
 for u, v, data in G.edges(data=True):
     x0, y0 = pos[u]
     x1, y1 = pos[v]
-    fig.add_trace(go.Scatter(x=[x0, x1], y=[y0, y1], mode='lines', line = dict(width=data['weight'] * 5, color='gray')))
+    fig.add_trace(go.Scatter(
+        x=[x0, x1], 
+        y=[y0, y1], 
+        mode='lines', 
+        line = dict(width=data['weight'] * 5, 
+            color='gray'),
+        hoverinfo='none'
+    ))
+
+# Labeling our detected nodes
+node_x = []
+node_y = []
+node_labels = []
 
 # Add nodes to the figure
 for node in G.nodes():
     x, y = pos[node]
-    fig.add_trace(go.Scatter(x=[x], y=[y], mode='markers', marker=dict(size=10)))
+    node_x.append(x)
+    node_y.append(y)
+    node_labels.append(node)
+
+fig.add_trace(go.Scatter(
+    x=node_x, 
+    y=node_y, 
+    mode='markers+text', 
+    marker=dict(size=10,
+        color='light-blue',
+        line=dict(width=2, color='black')
+    ),
+    text=node_labels,
+    textposition='top center',
+    textfont=dict(
+        size=12,
+        color='black'
+    ),
+    hoverinfo='text'
+    hovertext=node_labels
+))
+
+fig.update_layout(
+        title = 'Network Topology Visualizer',
+        title_x = 0.5,
+        showlegend = False,
+        hovermode = 'closest',
+        plot_bgcolor='white',
+        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False)
+        )
 
 # Show the figure
 fig.show()
